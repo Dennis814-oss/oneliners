235d260550df42b63ca72692027f42841efad6f2
LordA2117 <vip20dpa@gmail.com>
2025-01-02 15:13:44 +0530

    Added banners to the program

--- oneliners.py
+++ oneliners.py
@@ -1,5 +1,19 @@
 import random
 
+banner = r"""
+ _     _              _   _               _ 
+| |   (_)            | | | |             | |
+| |    _ _ __   ___  | |_| | __ _ _ __ __| |
+| |   | | '_ \ / _ \ |  _  |/ _` | '__/ _` |
+| |___| | | | |  __/ | | | | (_| | | | (_| |
+\_____/_|_| |_|\___| \_| |_/\__,_|_|  \__,_|
+"""
+
+print(banner)
+print("My favourite oneliners :)")
+print("Press any key to continue....")
+input()
+
 oneliners = [
     "I'll be back.",  # The Terminator (1984)
     "Yippee-ki-yay, mother****er!",  # Die Hard (1988)
